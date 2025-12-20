9bcc107f86e643512a9409f18110a3cac63a9e91
jshinkes00 <jshinkes00@gmail.com>
2025-12-20 12:46:28 +0900

    :첫 번째 배포: flask 서버 세팅

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return f"<h1>[로컬에서 수정함] 현재 서버 시간: {now}</h1>"
+
+if __name__ == '__main__':
+    app.run(host='0,0,0,0', port=5000)
