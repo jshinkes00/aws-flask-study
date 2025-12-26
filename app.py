f149a68e41a1f701c02805ea386a68d3f0239dae
jshinkes00 <jshinkes00@gmail.com>
2025-12-26 21:27:30 +0900

    멘트 수정

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return f"<h1>[로컬에서 수정함] 현재 서버 시간: {now}</h1>"
+    return f"<h1>[로컬에서 수정함] [자동화 코딩 완료] 현재 서버 시간: {now}</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
